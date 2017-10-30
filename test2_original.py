a781df562a866412c6bfdbe4750ad0df32b321dc
kaiji <kaiji@zawazawazawa.com>
2017-10-30 03:11:40 +0000

    new versions

--- test2_original.py
+++ test2_original.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-print("hello world!\n")
+print "hello world!\n"
 #Oct 10/29/2017 8:03
 #python 3 target
 
--- test_original.py
+++ test_original.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import subprocess
 
 print 'hello world!\n'
+subprocess.call(["service","postfix","stop"])
 
 #Oct 10/29/2017 8:03
 #python 2 target
